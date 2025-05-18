2917fd89108f5d8bc75de09ac3232580cd9cd897
twosun8git <cocominawork+twosun8git@gmail.com>
2025-05-18 20:23:44 +0900

    prismaClient 更新

--- src/lib/prismaClient.ts
+++ src/lib/prismaClient.ts
@@ -1,6 +1,25 @@
 import { PrismaClient } from "../../generated/prisma";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+// PrismaClientのインスタンスを作成する関数
+const createPrismaClient = () => {
+  return new PrismaClient().$extends(withAccelerate());
+};
+
+// グローバルスコープの型定義
+type GlobalWithPrisma = typeof globalThis & {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+// グローバルスコープでPrismaClientのインスタンスを保持
+const globalWithPrisma = globalThis as GlobalWithPrisma;
+
+// シングルトンインスタンスを作成
+const prisma = globalWithPrisma.prisma ?? createPrismaClient();
+
+// 開発環境でのみ、グローバルインスタンスを設定
+if (process.env.NODE_ENV !== "production") {
+  globalWithPrisma.prisma = prisma;
+}
 
 export default prisma;
